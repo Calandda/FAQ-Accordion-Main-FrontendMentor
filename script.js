f2d4613d77d10ab7da4693b109b0de993adfc649
Calandda <A.calandda@gmail.com>
2025-03-20 16:43:39 +0800

    Summary plus and minus image fix

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const divMain = document.querySelector('.divMain');
 divMain.addEventListener('click',(e)=>{
     if(e.target.tagName === 'SUMMARY'){
         if(e.target.parentNode.open === true){
-            e.target.style.backgroundImage = 'url("./assets/images/icon-plus.svg")'
+            e.target.style.backgroundImage = "url('./assets/images/icon-plus.svg')"
         } else if(e.target.parentNode.open === false){
-            e.target.style.backgroundImage = 'url("./assets/images/icon-minus.svg")';
+            e.target.style.backgroundImage = "url('./assets/images/icon-minus.svg')";
         }
     }
 });
